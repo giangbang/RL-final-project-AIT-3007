bbc47d5bd8f1f2bebd10c26e5c4761ac82bb6e1a
manhbd-22022602 <22022602@vnu.edu.vn>
2024-12-09 00:54:35 +0700

    0 step reward

--- train_qmix.py
+++ train_qmix.py
@@ -36,6 +36,8 @@ env = battle_v4.env(
     map_size=45,
     minimap_mode=False,
     extra_features=False,
+    step_reward=0,
+    dead_penalty=0,
 )
 env.reset()
 obs_dim = dummy_cnn.get_output_dim(env.observation_space("blue_0").shape[:-1])
