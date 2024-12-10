ffbf72736f60c8fd564fe89aa44e18d2be78cfda
manhbd-22022602 <22022602@vnu.edu.vn>
2024-12-11 02:24:57 +0700

    Fix print reward

--- qmix.py
+++ qmix.py
@@ -373,7 +373,7 @@ class QMix_Trainer():
         self.epsilon = max(self.epsilon_end, self.epsilon * self.epsilon_decay)
         self.agent.epsilon = self.epsilon
 
-        return loss.item(), reward.sum(dim=1)[0], env_rewards.sum(dim=1)[0], strategy_rewards.sum(dim=1)[0]
+        return loss.item(), reward.sum(dim=1)[0].cpu(), env_rewards.sum(dim=1)[0].cpu(), strategy_rewards.sum(dim=1)[0].cpu()
 
     def _build_td_lambda_targets(self, rewards, target_qs, gamma=0.99, td_lambda=0.6):
         '''
