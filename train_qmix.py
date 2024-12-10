153ba8ec63afe5cc58119e136924ca8fbb04970e
manhbd-22022602 <22022602@vnu.edu.vn>
2024-12-10 13:47:06 +0700

    Fix bug

--- train_qmix.py
+++ train_qmix.py
@@ -82,7 +82,7 @@ def train_blue_qmix(env, learner, max_episodes=1000, max_steps=200, batch_size=3
     learner.target_agent.train()
     learner.mixer.train()
     learner.target_mixer.train()
-    loss = None
+    loss, strategy_reward = None, None
     for episode in range(max_episodes):
         env.reset()
         episode_reward = 0
