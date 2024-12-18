a06d791389d6de256a1900a8a16058985a6c50a9
trnamanh12 <trnamanh8386@gmail.com>
2024-12-18 16:40:30 +0700

    add collate_episode (padded episodes) process episodes

--- amain/utils/process.py
+++ amain/utils/process.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pickle
+import torch
+from tensordict.tensordict import TensorDict
+from typing import List
 
 class Normalizer:
     def __init__(self, shape):
@@ -196,7 +199,7 @@ def process_batch(batchs, normalizer_obs_b: Normalizer=None, normalizer_obs_r: N
     for key in ['obs', 'actions', 'rewards', 'next_obs', 'dones', 'state', 'next_state', 'prev_actions']:
         max_len = max_length_red
         padded = []
-        masks = []
+        # masks = []
         for seq in batch_red[key]:
             pad_length = max_len - len(seq)
             if pad_length > 0:
@@ -208,8 +211,73 @@ def process_batch(batchs, normalizer_obs_b: Normalizer=None, normalizer_obs_r: N
                 padded_seq = seq
                 # mask = np.ones(len(seq))
             padded.append(padded_seq)
-            masks.append(mask)
+            # masks.append(mask)
         batch_red[key] = np.stack(padded, axis=0)
         # batch_red[f'{key}_mask'] = np.stack(masks, axis=0)
 
+    return batch_blue, batch_red
+
+def collate_episodes(episodes: List[TensorDict], device: torch.device):
+    """
+    Collate a list of episodes into batched TensorDict with padding.
+
+    Args:
+        episodes (List[TensorDict]): List of episodes to collate.
+        device (torch.device): Device to place tensors on.
+
+    Returns:
+        TensorDict: Batched episodes with padding and masks.
+    """
+    # Determine the maximum episode length in the batch
+    max_length = max(episode["o_b"].shape[0] for episode in episodes)
+
+    batched = {}
+    masks = torch.zeros(len(episodes), max_length, dtype=torch.bool)
+
+    for key in episodes[0].keys():
+        # Collect all steps for this key
+        key_data = [episode[key] for episode in episodes]
+        # Determine the shape excluding time
+        shape = key_data[0].shape[1:]
+        # Initialize batched tensor with padding
+        if key_data[0].dtype == torch.bool:
+            pad_value = False
+        elif key_data[0].dtype == torch.float32:
+            pad_value = 0.0
+        else:
+            pad_value = 0
+
+        batched[key] = torch.zeros(len(episodes), max_length, *shape)
+        batched[key].fill_(pad_value)
+
+        for i, episode in enumerate(key_data):
+            length = episode.shape[0]
+            batched[key][i, :length] = episode
+            masks[i, :length] = 1  # Mark valid steps
+
+    batched_tensordict = TensorDict(batched, batch_size=[len(episodes)])
+    batched_tensordict.set("mask", masks)
+    return batched_tensordict
+
+def process_episodes(batch):
+    batch_blue = {
+        'o' : batch['o_b'],
+        'a' : batch['a_b'],
+        'r' : batch['r_b'],
+        'n_o' : batch['n_o_b'],
+        'd' : batch['do'],
+        's' : batch['s'],
+        'n_s' : batch['n_s'],
+        'p_a' : batch['p_a_b']
+    }
+    batch_red = {
+        'o' : batch['o_r'],
+        'a' : batch['a_r'],
+        'r' : batch['r_r'],
+        'n_o' : batch['n_o_r'],
+        'd' : batch['do'],
+        's' : batch['s'],
+        'n_s' : batch['n_s'],
+        'p_a' : batch['p_a_r']
+    }
     return batch_blue, batch_red
